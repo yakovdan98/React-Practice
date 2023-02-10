762ebec92a4eabeba25dda7b7c5f7d8996cb7631
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-02-09 19:04:26 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setCount] = useState(0);
+  const [prevCount, setPrevCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((val) => {
+      setCount(val + 1);
+      setPrevCount(val);
+      console.log(val);
+    });
+  };
+
+  return (  
+    <>
+      <h1>Count: {count}</h1>
+      <h1>Previous Count: {prevCount}</h1>
+      <button onClick={handleClick}>+</button>
+    </>  
+   );
 }
+
 
 export default App;
